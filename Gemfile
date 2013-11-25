2e362ad6eb5bd171d565cb5c338634a6362d856f
Kovalevsky Dmitry <deecross33@gmail.com>
2013-11-25 19:30:55 +0400

    Unicorn configured, added some dev gems, set sass syntax

--- Gemfile
+++ Gemfile
@@ -4,15 +4,18 @@ gem 'rails', '4.0.1'
 
 gem 'pg', '0.17.0'
 
+
 gem 'sass-rails', '~> 4.0.0'
-gem 'uglifier', '>= 1.3.0'
+gem 'compass', '~> 0.12.2'
+gem 'zurb-foundation', '~> 4.3.2'
+gem 'haml'
 
 gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'turbolinks'
+gem 'uglifier', '>= 1.3.0'
 
 gem 'pry'
-gem 'rubocop'
 
 gem 'jbuilder', '~> 1.2'
 
@@ -23,7 +26,12 @@ end
 gem 'unicorn', '~> 4.7.0'
 
 group :development do
+  gem 'guard'
+  gem 'spork', '~> 1.0rc'
+
   gem 'capistrano', '~> 3.0.1'
   gem 'capistrano-rails', '~> 1.1.0'
   gem 'rvm-capistrano', '~> 1.5.0'
+  gem 'capistrano-bundler'
+  gem 'rubocop'
 end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,21 +1,31 @@
-rails_root = ENV["ROOT_EDUCAPSULE"]
+application = "educapsule"
+deploy_to  = "/home/cross/#{application}"
 
-worker_processes 4
+rails_root = "#{deploy_to}/current"
+socket_file = "/tmp/#{application}/sockets/.unicorn.sock"
+pid_file = "/tmp/#{application}/pids/unicorn.pid"
+error_log_file = "/tmp/#{application}/log/unicorn.stderr.log"
+output_log_file = "/tmp/#{application}/log/unicorn.stdout.log"
+old_pid    = "#{pid_file}.oldbin"
 
-working_directory rails_root + '/current'
+working_directory rails_root
 
-listen rails_root + "/current/tmp/sockets/.unicorn.sock", :backlog => 64
+listen socket_file, :backlog => 64
+pid pid_file
+
+stderr_path error_log_file
+stdout_path output_log_file
 
 timeout 30
-
-pid rails_root + "/current/tmp/pids/unicorn.pid"
-
-stderr_path rails_root + "/shared/log/unicorn.stderr.log"
-stdout_path rails_root + "/shared/log/unicorn.stdout.log"
+worker_processes 4
 
 preload_app true
-GC.respond_to?(:copy_on_write_friendly=) and
-    GC.copy_on_write_friendly = true
+
+GC.respond_to?(:copy_on_write_friendly=) and GC.copy_on_write_friendly = true
+
+before_exec do |server|
+  ENV["BUNDLE_GEMFILE"] = "#{rails_root}/Gemfile"
+end
 
 before_fork do |server, worker|
   defined?(ActiveRecord::Base) and
